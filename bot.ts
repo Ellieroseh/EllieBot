0c6156f92fc3858b650e4820bb740ae3fcc2e942
Ellie <ellie@scvle.com>
2021-08-23 18:12:03 +0100

    added pokemon roll card embed

--- bot.ts
+++ bot.ts
@@ -21,14 +21,16 @@ const client = new Client({
 });
 
 //Function that takes user input of pokemon name and returns info
-async function getPokemonByName(name: string) {
-	const response = await axios.get('https://pokeapi.co/api/v2/pokemon/' + name);
+async function getPokemonByName(pokemonName: string) {
+	const response = await axios.get(
+		'https://pokeapi.co/api/v2/pokemon/' + pokemonName
+	);
 	//Creating discord embed for pokemon card
-	const pokemonEmbed = new MessageEmbed()
+	const pokemonInfoEmbed = new MessageEmbed()
 		//Setting colour of sidebar to match image
 		.setColor('#f0bf62')
 		//Setting title
-		.setTitle(name)
+		.setTitle(pokemonName)
 		//Setting image
 		.setImage(response.data.sprites.other['official-artwork'].front_default)
 		//Fields which display information
@@ -43,7 +45,7 @@ async function getPokemonByName(name: string) {
 			}
 		)
 		.setFooter('© EllieBot');
-	return pokemonEmbed;
+	return pokemonInfoEmbed;
 }
 
 //Function that returns user by id from database
@@ -53,7 +55,7 @@ async function getUserById(uid: number) {
 		.orFail(new MongoDbError('User has not caught any pokemons yet ! :(', 404));
 }
 
-//Function that rolls a random pokemon and returns an embed of it
+//Function that rolls a random pokemon, logs it in the database and returns an embed of it
 async function rollPokemon(ownerId: number) {
 	//Generates a unique id for the pokemon in the collection
 	const pokemonId = uuidv1();
@@ -80,15 +82,30 @@ async function rollPokemon(ownerId: number) {
 		'https://pokeapi.co/api/v2/pokemon/' + pokedexId
 	);
 	//Weight multiplier is somewhere inbetween 0.8 and 1.2
-	const weightMultiplier = Math.floor(Math.random() * 120 + 80) / 100;
+	const weightMultiplier = Math.floor(Math.random() * 100 + 50) / 100;
 	//The weight of the pokemon is somewhere between 20% < average and 20% > average.
 	const weight = response.data.weight * weightMultiplier;
 	//Height multiplier is somewhere inbetween 0.8 and 1.2
-	const heightMultiplier = Math.floor(Math.random() * 120 + 80) / 100;
+	const heightMultiplier = Math.floor(Math.random() * 100 + 50) / 100;
 	//The height of the pokemon is somewhere between 20% < average and 20% > average.
 	const height = response.data.height * heightMultiplier;
+	//Selecting a random ability for the pokemon
+	const pokemonAbility =
+		response.data.abilities[
+			Math.floor(Math.random() * response.data.abilities.length)
+		];
+	//For each loop for RNG pokemon
+	const stats = response.data.stats.map((element) => {
+		const baseStat =
+			element.base_stat * (Math.floor(Math.random() * 100 + 50) / 100);
+		const statName = element.stat.name;
+		return {
+			[statName]: baseStat,
+		};
+	});
 
-	return await Pokemons.create({
+	//creating pokemon in database
+	await Pokemons.create({
 		pokemonId: pokemonId,
 		pokedexId: pokedexId,
 		ownerId: ownerId,
@@ -96,8 +113,112 @@ async function rollPokemon(ownerId: number) {
 		shiny: shiny,
 		weight: weight,
 		height: height,
+		stats: stats,
+		ability: pokemonAbility.ability.name,
 	});
+
+	//checking shiny/female
+	let pokemonImage: string = response.data.sprites.front_default;
+	//setting the image to shiny if pokemon is shiny
+	if (shiny) {
+		pokemonImage = response.data.sprites.front_shiny;
+	}
+	//setting image to female if the pokemon is female
+	if (gender === 'female' && response.data.sprites.front_female) {
+		pokemonImage = response.data.sprites.front_female;
+	}
+	//setting image to shiny female if pokemon is female and shiny
+	if (
+		gender === 'female' &&
+		shiny &&
+		response.data.sprites.front_shiny_female
+	) {
+		pokemonImage = response.data.sprites.front_shiny_female;
+	}
+	//Getting pokemon type
+	const types = response.data.types.map((element) => {
+		return element.type.name;
+	});
+	//Creating discord embed for pokemon roll card
+	const pokemonRollEmbed = new MessageEmbed()
+		//Setting colour of sidebar to match image
+		.setColor('#f0bf62')
+		//Setting title
+		.setTitle(response.data.name)
+		//Setting thumbnail image
+		.setThumbnail(pokemonImage)
+		//Fields which display information
+		.addFields(
+			{
+				name: 'Type(s):',
+				value: types.join(', '),
+				inline: true,
+			},
+			{
+				name: 'Gender:',
+				value: gender,
+				inline: true,
+			},
+			{
+				name: 'Shiny:',
+				value: shiny.toString(),
+				inline: true,
+			},
+			{
+				name: 'Weight:',
+				value: weight.toFixed(1) + 'kg',
+				inline: true,
+			},
+			{
+				name: 'Height:',
+				value: height.toFixed(1) + 'ft',
+				inline: true,
+			},
+			{
+				name: 'Ability:',
+				value: pokemonAbility.ability.name.split('-').join(' '),
+				inline: true,
+			},
+			{
+				name: 'Stats',
+				value: '',
+			},
+			{
+				name: 'HP:',
+				value: stats[0].hp,
+				inline: true,
+			},
+			{
+				name: 'Attack:',
+				value: stats[1].attack,
+				inline: true,
+			},
+			{
+				name: 'Defense:',
+				value: stats[2].defense,
+				inline: true,
+			},
+			{
+				name: 'Special Attack:',
+				value: stats[3]['special-attack'],
+				inline: true,
+			},
+			{
+				name: 'Special Defence:',
+				value: stats[4]['special-defense'],
+				inline: true,
+			},
+			{
+				name: 'Speed:',
+				value: stats[5].speed,
+				inline: true,
+			}
+		)
+		.setFooter('© EllieBot');
+	console.log(pokemonRollEmbed)
+	return pokemonRollEmbed;
 }
+
 
 //Console log that discord bot connected to discord
 client.once('ready', () => {
@@ -112,46 +233,51 @@ client.on('messageCreate', async (message) => {
 	switch (words[0]) {
 		case '!p':
 		case '!pokemon':
-			//if statement for when the !pokemon info is inputted
-			if (words[1] === 'info') {
-				try {
-					const pokemonEmbed = await getPokemonByName(words[2]);
-					message.reply({ embeds: [pokemonEmbed] });
-				} catch (error) {
-					message.reply('Pokemon not found idiot');
-				}
+			//when the !pokemon info is inputted
+			switch (words[1]) {
+				case 'info':
+					try {
+						const pokemonInfoEmbed = await getPokemonByName(words[2]);
+						message.reply({ embeds: [pokemonInfoEmbed] });
+					} catch (error) {
+						message.reply('Pokemon not found idiot');
+					}
+					break;
+				//when !pokemon collection is inputted
+				case 'collection':
+					try {
+						let uid: string;
+						//If no user is mentioned the user id is the id of the message author
+						if (!words[2]) {
+							uid = message.author.id;
+						}
+						//Otherwise the user id is the discord id of the user mentioned in the message
+						else {
+							const regexp = RegExp('<@!(?<uid>.+?)>');
+							const matches = words[2].match(regexp);
+							uid = matches.groups.uid;
+						}
+						const user = await getUserById(Number.parseInt(uid));
+					} catch (error) {
+						if (error instanceof MongoDbError) {
+							message.reply(error.message);
+						}
+					}
+					break;
+				case 'roll':
+					try {
+						const pokemonRollEmbed = await rollPokemon(
+							Number.parseInt(message.author.id)
+						);
+						message.reply({ embeds: [pokemonRollEmbed] });
+					} catch (error) {
+						if (error instanceof MongoDbError) {
+							message.reply(error.message);
+						}
+					}
+					break;
 			}
-			//If statement for the when !pokemon collection is inputted
-			if (words[1] === 'collection') {
-				try {
-					let uid: string;
-					//If no user is mentioned the user id is the id of the message author
-					if (!words[2]) {
-						uid = message.author.id;
-					}
-					//Otherwise the user id is the discord id of the user mentioned in the message
-					else {
-						const regexp = RegExp('<@!(?<uid>.+?)>');
-						const matches = words[2].match(regexp);
-						uid = matches.groups.uid;
-					}
-					const user = await getUserById(Number.parseInt(uid));
-					console.log(user);
-				} catch (error) {
-					if (error instanceof MongoDbError) {
-						message.reply(error.message);
-					}
-				}
-			}
-			if (words[1] === 'roll') {
-				try {
-					await rollPokemon(Number.parseInt(message.author.id));
-				} catch (error) {
-					if (error instanceof MongoDbError) {
-						message.reply(error.message);
-					}
-				}
-			}
+			break;
 	}
 	//if statement for gotta catch em all message
 	if (message.content.includes('pokemon')) {
--- schemas/Pokemons.ts
+++ schemas/Pokemons.ts
@@ -1,46 +1,59 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
 interface PokemonsTypes {
-    pokemonId: string;
-    ownerId: number;
-    pokedexId: number;
-    gender: string;
-    shiny: boolean;
-    height: string;
-    weight: string;
-    claimed: boolean;
+	pokemonId: string;
+	ownerId: number;
+	pokedexId: number;
+	gender: string;
+	shiny: boolean;
+	height: string;
+	weight: string;
+	claimed: boolean;
+	ability: string;
+	stats: [
+		{ hp: string },
+		{ attack: string },
+		{ defense: string },
+		{ 'special-attack': string },
+		{ 'special-defense': string },
+		{ speed: string }
+	];
 }
 
 //Pokemon Schema
-const Pokemons = new mongoose.Schema<PokemonsTypes>( 
-    {
-        "pokemonId": {
-          "type": "String"
-        },
-        "ownerId": {
-          "type": "Number"
-        },
-        "pokedexId": {
-          "type": "Number"
-        },
-        "gender": {
-          "type": "String"
-        },
-        "shiny": {
-          "type": "Boolean"
-        },
-        "height": {
-          "type": "String"
-        },
-        "weight": {
-          "type": "String"
-        },
-        "claimed": {
-          "type": "Boolean"
-        },
-      },
-      
-      { timestamps: true }
-      );
+const Pokemons = new mongoose.Schema<PokemonsTypes>(
+	{
+		pokemonId: {
+			type: 'String',
+		},
+		ownerId: {
+			type: 'Number',
+		},
+		pokedexId: {
+			type: 'Number',
+		},
+		gender: {
+			type: 'String',
+		},
+		shiny: {
+			type: 'Boolean',
+		},
+		height: {
+			type: 'String',
+		},
+		weight: {
+			type: 'String',
+		},
+		claimed: {
+			type: 'Boolean',
+		},
+		ability: {
+			type: 'String',
+		},
+		stats: ['Mixed'],
+	},
 
-export default mongoose.model<PokemonsTypes>("Pokemons", Pokemons);
+	{ timestamps: true }
+);
+
+export default mongoose.model<PokemonsTypes>('Pokemons', Pokemons);
